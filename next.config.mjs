2534e4135a952e91bac71ee2fceae7de2f19de6f
RapiSite Builder <builder@rapisite.com.br>
2026-02-23 22:18:46 +0000

    feat: site criado pelo RapiSite Builder

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['6ca3c31cd443b8133af7226009438b05.cdn.bubble.io', 'images.unsplash.com', 'i.pravatar.cc'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '6ca3c31cd443b8133af7226009438b05.cdn.bubble.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+      },
+    ],
   },
 };
 
